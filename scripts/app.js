303f525e51954b41b3238d4a7049bf5ecb995db1
Phil Fluegel <pfluegelcx@gmail.com>
2016-07-17 11:52:03 -0500

    added jquery for parallax

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+(function(){
+	$(window).scroll(function(){
+			var wScroll = $(this).scrollTop();
+			var windowHeight = $(window).height();
+			console.log(wScroll);
+			console.log('window height:  ' + windowHeight);
+			var diff = wScroll - windowHeight;
+			console.log('DIFF: ' + diff)
+		if(wScroll > 60){
+			$('.hidden-nav img').css({'bottom' : -67 + 'px'});
+		}else{
+			$('.hidden-nav').removeClass('show');
+			$('.hidden-nav img').css({'bottom' : 0});
+		}
+		if(wScroll > 0){
+			var marginTop = -30;
+			$('#clouds').css({'top':  ((wScroll) / 2)})
+			$('.about').css({'margin-top':  marginTop - ((wScroll) / 4)})
+		}
+	});
+	
+	
+	$('.hidden-nav img').click(function(){
+		$('.hidden-nav').toggleClass('show');
+	});
+	
+	
+	
+	
+})();
