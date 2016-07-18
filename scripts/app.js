fba1d29a5aad7db0cdba905d2e22e1713a035520
Phil Fluegel <pfluegelcx@gmail.com>
2016-07-17 19:13:34 -0500

    more parallax

--- scripts/app.js
+++ scripts/app.js
@@ -4,8 +4,8 @@
 			var windowHeight = $(window).height();
 			console.log(wScroll);
 			console.log('window height:  ' + windowHeight);
-			var diff = wScroll - windowHeight;
-			console.log('DIFF: ' + diff)
+			var diff = windowHeight - wScroll;
+			console.log('DIFF: ' + diff);
 		if(wScroll > 60){
 			$('.hidden-nav img').css({'bottom' : -67 + 'px'});
 		}else{
@@ -14,10 +14,31 @@
 		}
 		if(wScroll >= 0){
 			var marginTop = -40;
-			$('#clouds').css({'top':  ((wScroll) / 2)})
-			$('.about').css({'margin-top':  marginTop - ((wScroll) / 4)})
+			$('#clouds').css({'top':  ((wScroll) / 2)});
+			$('.about').css({'margin-top':  marginTop - ((wScroll) / 4)});
 		}
+		if(wScroll > $('.info-images').offset().top - windowHeight / 1.2){
+				$('.image').each(function(i){
+					setTimeout(function(){
+						$('.image').eq(i).addClass('trans-show');	
+					},200 * (i + 1));
+				});
+		}if(wScroll < 100){
+			$('.image').removeClass('trans-show');
+		}
+		
+		if(wScroll > $('.info-images').offset().top - windowHeight / 1.7){
+						$('.info-div').addClass('slide-up');	
+			}if(wScroll < 100){
+				$('.info-div').removeClass('slide-up');	
+			}
+		
 	});
+	
+	
+	
+	
+	
 	
 	
 	$('.hidden-nav img').click(function(){
@@ -25,6 +46,6 @@
 	});
 	
 	
-	
+	//$('.image').addClass('trans-show');
 	
 })();
